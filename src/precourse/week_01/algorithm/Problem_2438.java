d65575ab34c12e8857afa0f57e3a489caeaeffde
KOKEONHO <rhrjsgh97@gmail.com>
2022-11-03 14:06:20 +0900

    IntelliJ 1st commit test

--- src/precourse/week_01/algorithm/Problem_2438.java
+++ src/precourse/week_01/algorithm/Problem_2438.java
@@ -1,17 +1,24 @@
-package mission01;
+package precourse.week_01.algorithm;
 
 import java.io.*;
 
-public class Main {
+public class Problem_2438 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
+        br.close();
 
-        for (int i = N; i > 0; i--) {
-            
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<=i; j++) {
+                bw.write("*");
+                bw.flush();
+            }
+            bw.newLine();
         }
+
+        bw.close();
     }
 }
---
+++ src/precourse/week_01/algorithm/Solution.java
@@ -0,0 +1,34 @@
+//package precourse.week_01.algorithm;
+//
+//import java.io.BufferedWriter;
+//import java.io.IOException;
+//import java.io.OutputStreamWriter;
+//
+//// 메서드 나누는 연습 해볼 것 !!!!
+//
+//public class Solution {
+//    public static void main(String[] args) throws IOException {
+//
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        String[] babbling = {"aya","ye","woo","ma"};    // 조카가 발음할 수 있는 발음들의 배열
+//
+//        int result = solution(babbling);
+//
+//        bw.write(Integer.toString(result)); // 발음할 수 있는 단어의 갯수 출력
+//
+//        bw.flush();
+//        bw.close();
+//    }
+//
+//    public int solution(String[] babbling) {
+//
+//        String checkStr;    // 확인해야 할 발음 변수
+//        String[] array = {"aya","yee","u","maa","wyeoo"};   // 확인해야 할 발음들의 배열
+//
+//        for(int i=0; i<array.length; i++) {
+//            checkStr = array[i];
+//
+//        }
+//    }
+//}
