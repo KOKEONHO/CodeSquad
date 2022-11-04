827cd4f514ae69bb4527afd19ac55a2328dfe1c3
KOKEONHO <rhrjsgh97@gmail.com>
2022-11-05 05:22:08 +0900

    GitHub usage practice

---
+++ src/precourse/week_01/mission02/question04/MyDate.java
@@ -0,0 +1,27 @@
+//package precourse.week_01.mission02.question04;
+//
+//public class MyDate {
+//
+//    private int day;
+//    private int month;
+//    private int year;
+//
+//    MyDate(int day, int month, int year) {
+//        this.day = day;
+//        this.month = month;
+//        this.year = year;
+//    }
+//
+//    boolean checkLeap(int year) {   // 윤년 체크
+//        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//
+//
+//    public String isValid() {
+//
+//    }
+//}
---
+++ src/precourse/week_01/mission02/question04/MyDateTest.java
@@ -0,0 +1,18 @@
+//package precourse.week_01.mission02.question04;
+//
+//import java.io.BufferedWriter;
+//import java.io.IOException;
+//import java.io.OutputStreamWriter;
+//
+//public class MyDateTest {
+//    public static void main(String[] args) throws IOException {
+//
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        MyDate date1 = new MyDate(30,2,2000);
+//        bw.write(date1);
+//
+//        MyDate date2 = new MyDate(2,10,2006);
+//
+//    }
+//}
