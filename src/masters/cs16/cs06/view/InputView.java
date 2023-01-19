b34c077cdfc5e8fbb72cd31a59d353bbaf55749e
KOKEONHO <rhrjsgh97@gmail.com>
2023-01-19 10:23:33 +0900

    Feat: "InputView.java 추가"
    체스 게임에서 사용자의 입력을 담당할 InputView.java 추가

---
+++ src/masters/cs16/cs06/view/InputView.java
@@ -0,0 +1,20 @@
+package masters.cs16.cs06.view;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputView {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public String globalReadLine() {
+        String input = "";
+        try {
+            input = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return input;
+    }
+}
